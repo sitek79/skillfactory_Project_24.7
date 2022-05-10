f07dab65545c5927f2e6fcb25ff366144273d2ae
sitek79 <atikhonov79@gmail.com>
2022-05-10 18:23:40 +0300

    10may2022_Part_7_Branch_project_29.5_Add_logging_properties_Edit_3

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,8 +2,13 @@ import comparator.StudentComparator;
 import comparator.UniversityComparator;
 import enums.StudentComparatorType;
 import enums.UniversityComparatorType;
+import io.JsonWriter;
 import io.ReadXLSXFile;
+import io.WriteJSON;
+import io.WriteXMLfile;
 import io.WriteXlsFile;
+import io.XmlWriter;
+import model.FullInfo;
 import model.Statistics;
 import model.Student;
 import model.University;
@@ -12,52 +17,49 @@ import util.JsonUtil;
 import util.StatisticsUtil;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
 
-        List<University> universities =
-                ReadXLSXFile.readXlsUniversities("src/main/resources/universityInfo.xlsx");
+    private static final Logger logger = Logger.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+
+        try {
+            LogManager.getLogManager().readConfiguration(Application.class.getResourceAsStream("/logging.properties"));
+        } catch (IOException e) {
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+        logger.log(INFO, "Application started, Logger configured");
+
+        //
+        List<University> universities = ReadXLSXFile.readXlsUniversities("src/main/resources/universityInfo.xlsx");
         UniversityComparator universityComparator =
                 ComparatorUtil.getUniversityComparator(UniversityComparatorType.YEAR);
         universities.sort(universityComparator);
-        String universitiesJson = JsonUtil.universityListToJson(universities);
-        // проверяем, что json создан успешно
-        System.out.println(universitiesJson);
-        List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(universitiesJson);
-        // проверяем, что обратно коллекция воссоздаётся в таком же количестве элементов
-        System.out.println(universities.size() == universitiesFromJson.size());
-        universities.forEach(university -> {
-            String universityJson = JsonUtil.universityToJson(university);
-            // проверяем, что json из отдельного элемента создан успешно
-            System.out.println(universityJson);
-            University universityFromJson = JsonUtil.jsonToUniversity(universityJson);
-            // проверяем, что обратно элемент воссоздаётся
-            System.out.println(universityFromJson);
-        });
 
-        List<Student> students =
-                ReadXLSXFile.readXlsStudents("src/main/resources/universityInfo.xlsx");
+        List<Student> students = ReadXLSXFile.readXlsStudents("src/main/resources/universityInfo.xlsx");
         StudentComparator studentComparator =
                 ComparatorUtil.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE);
         students.sort(studentComparator);
-        String studentsJson = JsonUtil.studentListToJson(students);
-        // проверяем, что json создан успешно
-        System.out.println(studentsJson);
-        List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
-        // проверяем, что обратно коллекция воссоздаётся в таком же количестве элементов
-        System.out.println(students.size() == studentsFromJson.size());
-        students.forEach(student -> {
-            String studentJson = JsonUtil.studentToJson(student);
-            // проверяем, что json из отдельного элемента создан успешно
-            System.out.println(studentJson);
-            Student studentFromJson = JsonUtil.jsonToStudent(studentJson);
-            // проверяем, что обратно элемент воссоздаётся
-            System.out.println(studentFromJson);
-        });
 
         List<Statistics> statisticsList = StatisticsUtil.createStatistics(students, universities);
         WriteXlsFile.writeXlsStatistics(statisticsList, "statistics.xlsx");
+
+        FullInfo fullInfo = new FullInfo()
+                .setStudentList(students)
+                .setUniversityList(universities)
+                .setStatisticsList(statisticsList)
+                .setProcessDate(new Date());
+
+        WriteXMLfile.generateXmlReq(fullInfo);
+        WriteJSON.writeJsonReq(fullInfo);
+
+        logger.log(INFO, "Application finished");
     }
 }
--- src/main/java/util/StatisticsUtil.java
+++ src/main/java/util/StatisticsUtil.java
@@ -12,13 +12,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class StatisticsUtil {
+    private static final Logger logger = Logger.getLogger(StatisticsUtil.class.getName());
+
     private StatisticsUtil() {
     }
 
-    public static List<Statistics> createStatistics(List<Student> students, List<University> universities) {
+    public static List<Statistics> createStatistics(List<Student> students,
+                                                    List<University> universities) {
+
+        logger.log(Level.INFO, "Statistics module started");
 
         List<Statistics> statisticsList = new ArrayList<>();
 
@@ -53,6 +60,9 @@ public class StatisticsUtil {
             avgExamScore.ifPresent(value -> statistics.setAvgExamScore(
                     (float) BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP).doubleValue()));
         });
+
+        logger.log(Level.INFO,
+                String.format("Statistics module finished with %s statistical objects", statisticsList.size()));
 
         return statisticsList;
     }
